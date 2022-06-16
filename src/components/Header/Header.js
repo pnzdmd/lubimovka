5c23cc3a76fe734aa2070c25ad31a59d22eb210e
Iana Pylaeva <MPiY@yandex.ru>
2022-06-16 20:57:06 +0300

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,21 +11,21 @@ function Header() {
       <a href="#" target="_blank" rel="noopener" className="header__logo"><img src={logo} alt="Логотип Любимовка" /></a>
       <div className="header__menu">
         <>
-          <ButtonHeaderMenu title="Афиша" />
-          <ButtonHeaderMenu title="Библиотека" />
-          <ButtonHeaderMenu title="Проекты" />
-          <ButtonHeaderMenu title="История" />
-          <ButtonHeaderMenu title="Блог" />
-          <ButtonHeaderMenu title="Новости" />
-          <ButtonHeaderMenu title="О фестивале" />
-          <ButtonHeaderMenu title="Контакты" />
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="Афиша" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="Библиотека" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="Проекты" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="История" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="Блог" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="Новости" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="О фестивале" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-menu-button"><ButtonHeaderMenu title="Контакты" /></a>
         </>
         <div className="header__links">
-          <ButtonHeaderLink title="fb" />
-          <ButtonHeaderLink title="inst" />
-          <ButtonHeaderLink title="ytube" />
-          <ButtonHeaderLink title="tlgrm" />
-          <ButtonHeaderLink title="vk" />
+          <a href="#" target="_blank" rel="noopener" className="header__link-around-button"><ButtonHeaderLink title="fb" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-around-button"><ButtonHeaderLink title="inst" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-around-button"><ButtonHeaderLink title="ytube" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-around-button"><ButtonHeaderLink title="tlgrm" /></a>
+          <a href="#" target="_blank" rel="noopener" className="header__link-around-button"><ButtonHeaderLink title="vk" /></a>
         </div>
         <img className="header__button-plus" src={buttonPlus} alt="Логотип Любимовка" /><div className="header__button-support">Поддержать</div>
       </div>
